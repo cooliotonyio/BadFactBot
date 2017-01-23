8825b92bc1d55fbc0ad1b9c591dcd3ec4ea25bd4
Tony Zhao <tzhao@airbears2-10-142-78-61.airbears2.1918.berkeley.edu>
2017-01-23 13:26:40 -0800

    Changed to every day

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import tweepy, time, sys, WikiScraper, FactGenerator, keys
 
 api = keys.api
 
-while True:
+for k in range(100):
 	try:
 		line = FactGenerator.create_fact()
-		print('*'*20)
-		print(time.strftime("%D %H:%M%PM"))
+		print('*'*40)
+		print('Tweet number',k+1,'of 100')
+		print(time.strftime("%D %H:%M%P"))
 		print('Tweeting:')
 		print(line)
 		api.update_status(line)
-		for i in range(7):
-			time.sleep(60*60*24)
-			print('Days before next tweet:',7-i)
-	except:
+		for j in range(24):
+			print(24-j,'hours left before tweet #',k+2)
+			time.sleep(3600)
+	except UnicodeEncodeError:
 		print("Bad UTF-8 and ASCII encoding")
+		print('Trying again...')
